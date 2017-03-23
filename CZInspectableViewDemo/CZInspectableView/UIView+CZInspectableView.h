7e6afe268cba9c185f94e643019e6f69bd5f2fea
Clay Zhu <clayzhu@163.com>
2017-03-23 17:14:35 +0800

    新建 Category。

---
+++ CZInspectableViewDemo/CZInspectableView/UIView+CZInspectableView.h
@@ -0,0 +1,13 @@
+//
+//  UIView+CZInspectableView.h
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CZInspectableView)
+
+@end
---
+++ CZInspectableViewDemo/CZInspectableView/UIView+CZInspectableView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+CZInspectableView.m
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "UIView+CZInspectableView.h"
+
+@implementation UIView (CZInspectableView)
+
+@end
