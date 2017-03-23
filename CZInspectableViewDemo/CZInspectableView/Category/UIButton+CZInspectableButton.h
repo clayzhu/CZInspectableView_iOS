2494ab4d81c888ec2cb4af79138af0e379260d25
Clay Zhu <clayzhu@163.com>
2017-03-23 18:06:38 +0800

    新建 CZInspectableButton 分类。

---
+++ CZInspectableViewDemo/CZInspectableView/Category/UIButton+CZInspectableButton.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+CZInspectableButton.h
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (CZInspectableButton)
+
+@end
---
+++ CZInspectableViewDemo/CZInspectableView/Category/UIButton+CZInspectableButton.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+CZInspectableButton.m
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "UIButton+CZInspectableButton.h"
+
+@implementation UIButton (CZInspectableButton)
+
+@end
