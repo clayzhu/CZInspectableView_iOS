57b8d08dec69947475e2095cfa3a1f6a094d0fea
Clay Zhu <clayzhu@163.com>
2017-03-23 17:58:33 +0800

    添加 UITextField 的子类 CZInspectableTextField，调用即可展现修改 placeholder 颜色的效果。

---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableTextField.h
@@ -0,0 +1,13 @@
+//
+//  CZInspectableTextField.h
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZInspectableTextField : UITextField
+
+@end
---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableTextField.m
@@ -0,0 +1,21 @@
+//
+//  CZInspectableTextField.m
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZInspectableTextField.h"
+
+@implementation CZInspectableTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
