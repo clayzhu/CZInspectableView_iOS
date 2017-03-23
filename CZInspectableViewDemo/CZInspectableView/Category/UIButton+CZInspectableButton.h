60663ab3a3923c932e2d0a34da5aedadfa8418b2
Clay Zhu <clayzhu@163.com>
2017-03-23 18:47:14 +0800

    实现 CZInspectableButton 分类的属性。Demo.

--- CZInspectableViewDemo/CZInspectableView/Category/UIButton+CZInspectableButton.h
+++ CZInspectableViewDemo/CZInspectableView/Category/UIButton+CZInspectableButton.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ UIButton 的 Category CZInspectableButton，添加 UIButton 专有的属性修改，而 UIView 共有的属性已经通过 CZInspectableView Category 自动实现。
+ */
 @interface UIButton (CZInspectableButton)
+
+/** 按钮 UIControlStateNormal 时的背景颜色 */
+@property (strong, nonatomic) IBInspectable UIColor *backgroundColorForNormal;
+/** 按钮 UIControlStateHighlighted 时的背景颜色 */
+@property (strong, nonatomic) IBInspectable UIColor *backgroundColorForHighlighted;
+/** 按钮 UIControlStateDisabled 时的背景颜色 */
+@property (strong, nonatomic) IBInspectable UIColor *backgroundColorForDisabled;
+/** 按钮 UIControlStateSelected 时的背景颜色 */
+@property (strong, nonatomic) IBInspectable UIColor *backgroundColorForSelected;
 
 @end
