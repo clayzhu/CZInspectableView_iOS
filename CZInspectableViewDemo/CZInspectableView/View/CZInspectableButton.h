2d6ce48f1ed4f740e2408d767d5044ad8d7d5417
Clay Zhu <clayzhu@163.com>
2017-03-23 18:07:38 +0800

    新建 UIButton 子类 CZInspectableButton。

---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableButton.h
@@ -0,0 +1,13 @@
+//
+//  CZInspectableButton.h
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZInspectableButton : UIButton
+
+@end
---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableButton.m
@@ -0,0 +1,21 @@
+//
+//  CZInspectableButton.m
+//  CZInspectableViewDemo
+//
+//  Created by Apple on 2017/3/23.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZInspectableButton.h"
+
+@implementation CZInspectableButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
