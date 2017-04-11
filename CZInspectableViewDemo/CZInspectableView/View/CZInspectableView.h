d9f05b5d0482efc2257926b821c8416067b3c7d2
Clay Zhu <clayzhu@163.com>
2017-04-11 23:29:08 +0800

    新建继承 UIView 的子类。

---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableView.h
@@ -0,0 +1,13 @@
+//
+//  CZInspectableView.h
+//  CZInspectableViewDemo
+//
+//  Created by ug19 on 2017/4/11.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZInspectableView : UIView
+
+@end
---
+++ CZInspectableViewDemo/CZInspectableView/View/CZInspectableView.m
@@ -0,0 +1,21 @@
+//
+//  CZInspectableView.m
+//  CZInspectableViewDemo
+//
+//  Created by ug19 on 2017/4/11.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZInspectableView.h"
+
+@implementation CZInspectableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
