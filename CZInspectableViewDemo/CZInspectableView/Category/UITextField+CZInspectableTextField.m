ff3cd37343e5f4e239168887c431b45c75876f7f
Clay Zhu <clayzhu@163.com>
2017-04-12 23:10:48 +0800

    UITextField 分类添加文字和边框的间距属性。

--- CZInspectableViewDemo/CZInspectableView/Category/UITextField+CZInspectableTextField.m
+++ CZInspectableViewDemo/CZInspectableView/Category/UITextField+CZInspectableTextField.m
@@ -18,4 +18,24 @@
 	[self setValue:placeholderColor forKeyPath:@"_placeholderLabel.textColor"];
 }
 
+- (CGFloat)leadingSpacing {
+    return CGRectGetWidth(self.leftView.bounds);
+}
+
+- (void)setLeadingSpacing:(CGFloat)leadingSpacing {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, leadingSpacing, self.bounds.size.height)];
+    self.leftView = view;
+    self.leftViewMode = UITextFieldViewModeAlways;
+}
+
+- (CGFloat)taillingSpacing {
+    return CGRectGetWidth(self.rightView.bounds);
+}
+
+- (void)setTaillingSpacing:(CGFloat)taillingSpacing {
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, taillingSpacing, self.bounds.size.height)];
+    self.rightView = view;
+    self.rightViewMode = UITextFieldViewModeAlways;
+}
+
 @end
